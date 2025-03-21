bc527df1b0859c832fc93e27b28b1d25f300ff6c
liamfrogspark <rafael.cardoso@thedigitalmaze.com>
2025-03-21 13:15:46 +0000

    adding serializer

--- components/CustomPortableText.js
+++ components/CustomPortableText.js
@@ -3,30 +3,39 @@ import slugify from "slugify";
 import Link from "next/link";
 
 const customSerializers = {
-	link: (props) => {
-		const { blank, href, children } = props
-		return blank ?
-				<a href={href} target="_blank" rel="noopener">{children}</a>
-				: <a href={href}>{children}</a>
-	},
-	mailToLink: (props) => {
-		const {email, children} = props;
-		const link = `mailto:${email}`;
-		return <a href={ link }>{children}</a>
-	}
-}
+  block: (props) => {
+    const { node, children } = props;
+    const anchorId = node.anchorId || slugify(children?.[0] || "", { lower: true, strict: true });
 
-const CustomPortableText = ( {content, className, serializers} ) => {
-	const mergedSerializers = { ...customSerializers, ...serializers};
-	console.log(mergedSerializers);
+    return (
+      <p id={anchorId} className="scroll-mt-20">
+        {children}
+      </p>
+    );
+  },
+  link: (props) => {
+    const { blank, href, children } = props;
+    return blank ? (
+      <a href={href} target="_blank" rel="noopener">
+        {children}
+      </a>
+    ) : (
+      <a href={href}>{children}</a>
+    );
+  },
+  mailToLink: (props) => {
+    const { email, children } = props;
+    const link = `mailto:${email}`;
+    return <a href={link}>{children}</a>;
+  },
+};
 
-	return (
-			<PortableText
-					className={className}
-					content={content}
-					serializers={ mergedSerializers }
-			/>
-	)
-}
+const CustomPortableText = ({ content, className, serializers }) => {
+  const mergedSerializers = { ...customSerializers, ...serializers };
 
-export default CustomPortableText
+  return (
+    <PortableText className={className} content={content} serializers={mergedSerializers} />
+  );
+};
+
+export default CustomPortableText;
