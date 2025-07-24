b545847ac74654bb53becdbb797c38ee90117be6
Calum Ogilvie <calum.ogilvie@thedigitalmaze.com>
2025-07-24 10:03:14 +0100

    adding/removing logging

--- components/CustomPortableText.js
+++ components/CustomPortableText.js
@@ -25,178 +25,8 @@ const scrollToAnchor = (id) => {
   }
 };
 
-// const customSerializers = {
-//   block: (props) => {
-//     console.log('block');
-//     const { children, node } = props;
-//     // console.log(node);
-//     const style = node?.style || "normal";
-//
-//     let anchorId = "";
-//     let anchorLink = "";
-//     let plainText = "";
-//
-//     if (Array.isArray(children)) {
-//       children.forEach((child) => {
-//         if (typeof child === "string") {
-//           plainText += child + " ";
-//         } else if (child?.props?.node?.mark?._type === "anchorId") {
-//           anchorId = child.props.node.mark.anchorId;
-//         } else if (child?.props?.node?.mark?._type === "anchorLink") {
-//           anchorLink = child.props.node.mark.href;
-//         }
-//       });
-//     }
-//     const tagMap = {
-//       h1: "h1",
-//       h2: "h2",
-//       h3: "h3",
-//       h4: "h4",
-//       h5: "h5",
-//       h6: "h6",
-//       normal: "p",
-//       blockquote: "blockquote",
-//     };
-//
-//     const Tag = tagMap[style] || "p";
-//
-//     return (
-//         <Tag className="scroll-mt-20" id={anchorId || undefined}>
-//           {children.map((child, index) => {
-//             if (
-//                 typeof child === "object" &&
-//                 child?.props?.node?.mark &&
-//                 child.props.node.mark._type
-//             ) {
-//               const mark = child.props.node.mark;
-//
-//               if (mark._type === "anchorLink") {
-//                 return (
-//                     <a
-//                         key={index}
-//                         href={"#" + mark.href}
-//                         onClick={(e) => {
-//                           e.preventDefault();
-//                           scrollToAnchor(mark.href);
-//                         }}
-//                         className="underline anchor-link"
-//                     >
-//                       {child}
-//                     </a>
-//                 );
-//               }
-//
-//               if (mark._type === "anchorId") {
-//                 return (
-//                     <span key={index} className="anchor">
-//                   {child}
-//                 </span>
-//                 );
-//               }
-//             }
-//
-//             return child;
-//           })}
-//         </Tag>
-//     );
-//   },
-//
-//   marks: {
-//     anchorLink: ({ mark, children }) => {
-//       console.log('anchor link');
-//       return (
-//         <a
-//           href={"#" + mark.href}
-//           onClick={(e) => {
-//             e.preventDefault();
-//             scrollToAnchor(mark.href);
-//           }}
-//           className="new-anchor-link"
-//         >
-//           {children}
-//         </a>
-//       );
-//     },
-//
-//     anchorId: ({ mark, children }) => {
-//       return (
-//         <span id={mark.anchorId} className="anchor">
-//           {children}
-//         </span>
-//       );
-//     },
-//
-//     link: ({ mark, children }) => {
-//       console.log('mark link');
-//
-//       return mark.blank ? (
-//         <a href={mark.href} target="_blank" rel="noopener noreferrer">
-//           {children}
-//         </a>
-//       ) : (
-//         <a href={mark.href}>{children}</a>
-//       );
-//     },
-//
-//     mailToLink: (props) => {
-//       console.log('mailto')
-//       const { email, children } = props;
-//       const link = `mailto:${email}`;
-//       return <a href={link}>{children}</a>;
-//     },
-//   },
-//
-//   types: {
-//     Embed: (props) => {
-//       console.log('EMBED')
-//       console.log(props);
-//
-//       return '';
-//     },
-//
-//     link: (props) => {
-//       console.log('normal link');
-//       const { blank, href, children } = props;
-//       return blank ? (
-//           <a href={href} target="_blank" rel="noopener">
-//             {children}
-//           </a>
-//       ) : (
-//           <a href={href}>{children}</a>
-//       );
-//     },
-//
-//     Quote: (props) => <Blockquote quote={props.quote} author={props.author} />,
-//     Image: (props) => {
-//       return (
-//           <SanityImageResponsive
-//               image={props.image}
-//               wrap={props.wrapText}
-//               customLink={props.customLink}
-//           />
-//       )
-//     },
-//     internalLink: (props) => {
-//       console.log('internal link');
-//       const {slug = {}} = props
-//
-//       // Prefix
-//       let prefix = '/'
-//       props.type == 'categories' && (prefix = '/news/categories/')
-//       props.type == 'news' && (prefix = '/news/')
-//       props.type == 'policies' && (prefix = '/policies/')
-//
-//       // HREF
-//       const href = `${prefix}${ slug ? slug.current : slugify(JSON.stringify(props.title), { lower: true, remove: /[*+~.()'"!:@]/g})}`
-//
-//       return <Link href={href}>{props.children}</Link>
-//     }
-//   }
-// };
-
 const customSerializers = {
   anchorLink: (props) => {
-    console.log('anchor link');
     return (
         <a
             href={"#" + props.href}
@@ -212,8 +42,6 @@ const customSerializers = {
   },
 
   anchorId: (props) => {
-    console.log('aid mark');
-    console.log(props);
     return (
         <span id={props.anchorId} className="anchor">
         {props.children}
@@ -222,7 +50,6 @@ const customSerializers = {
   },
 
   mailToLink: (props) => {
-    console.log('mailto')
     const { email, children } = props;
     const link = `mailto:${email}`;
     return <a href={link}>{children}</a>;
@@ -231,6 +58,9 @@ const customSerializers = {
   Embed: (props) => <div className="w-full" dangerouslySetInnerHTML={{__html: props.code}} />,
 
   Image: (props) => {
+    console.log('image');
+    console.log(props);
+
     return (
         <SanityImageResponsive
             image={props.image}
@@ -241,7 +71,6 @@ const customSerializers = {
   },
 
   internalLink: (props) => {
-    console.log('internal link');
     const {slug = {}} = props
 
     // Prefix
@@ -257,7 +86,6 @@ const customSerializers = {
   },
 
   link: (props) => {
-    console.log('normal link');
     const { blank, href, children } = props;
     return blank ? (
         <a href={href} target="_blank" rel="noopener noreferrer">
@@ -279,12 +107,6 @@ const CustomPortableText = ({ content, className, serializers }) => {
   
   // Otherwise, process it as a Portable Text object.
   const mergedSerializers = { ...customSerializers, ...serializers };
-
-  console.log('merged');
-  console.log(mergedSerializers);
-
-  console.log('content');
-  console.log(content);
 
   return (
     <PortableText
